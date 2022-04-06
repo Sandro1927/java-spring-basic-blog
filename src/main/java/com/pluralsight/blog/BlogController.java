1b59d62e9ab9eb7d8229a10ac9b048e31042e84e
alessandro.borelli <AF54040@enel.com>
2022-04-06 17:50:45 +0200

    fix

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,17 +1,30 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    @Autowired
+    private PostRepository postRepository;
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
+        List<Post> postList = postRepository.getAllPosts();
+      //  modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts",postList);
         return "home";
     }
- }
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+}
 
 
